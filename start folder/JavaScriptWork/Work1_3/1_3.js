6dd3771c43071da4a639af217e39e52f318390f8
masafumi kondo <acm1899x@outlook.com>
2025-04-06 20:28:40 +0900

    m

--- start folder/JavaScriptWork/Work1_3/1_3.js
+++ start folder/JavaScriptWork/Work1_3/1_3.js
@@ -1,14 +1,1 @@
-console.log("worx外部参照")
-
-let getTotalPrice = 　 => price * quantity;
-
-const addTax = total => getTotalPrice * 1.1;
-
-const total = getTotalPrice(1000, 2); // → 2000
-
-console.log(　　);
-
-const  = addTax(total);     // → 2200
-
-console.log(`税込金額は${taxedTotal}円です`);
 
