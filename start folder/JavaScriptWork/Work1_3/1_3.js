d99a471d26ddf4a976f5848a5f448ce858245152
masafumi kondo <acm1899x@outlook.com>
2025-04-07 05:26:24 +0900

    js課題

--- start folder/JavaScriptWork/Work1_3/1_3.js
+++ start folder/JavaScriptWork/Work1_3/1_3.js
@@ -1,1 +1,19 @@
+// function getTotalPrice(price, quantity) {
+//     return price * quantity;
+// }
+// const TotalPrice = getTotalPrice(1000, 2)
+// console.log(`税抜金額は${TotalPrice}円です`);
 
+// function addTax(TotalPrice, tax) {
+//     return TotalPrice * tax;
+// }
+// const taxedTotal = addTax(TotalPrice, 1.1); 
+// console.log(`税込金額は${taxedTotal}円です`);
+
+const getTotalPrice = (price, quantity ) =>  price * quantity;
+const addTax = (total) => total * 1.1;
+// なぜ引数として 1.1がないのか,()がなく混乱した。
+const total = getTotalPrice(1000, 2); // → 2000
+console.log(`税抜金額は${total}円です`);
+const taxedTotal = addTax(total, 1.1);     // → 2200
+console.log(`税込金額は${taxedTotal}円です`);
