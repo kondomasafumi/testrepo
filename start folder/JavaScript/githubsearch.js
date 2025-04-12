af5fe323d735a700148d1a11f342472256975360
masafumi kondo <acm1899x@outlook.com>
2025-04-12 22:25:46 +0900

    GITAPI検索

---
+++ start folder/JavaScript/githubsearch.js
@@ -0,0 +1,50 @@
+const usernameInput = document.getElementById("username")
+const clickBtn = document.getElementById("searchbtn");
+const resultArea = document.getElementById("result");
+
+clickBtn.addEventListener("click", clicksearch);
+function clicksearch() {
+    const username = usernameInput.value;
+    const contents = resultArea.textContent
+    console.log("取得したユーザー名:", username);
+    const url = `https://api.github.com/users/${username}`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status:${response.status}`);
+            }
+            return response.json();
+        }
+        )
+        .then(date =>
+            resultArea.innerHTML =
+            `<p>ユーザーID：${date.login}</p>
+            <p>ユーザー名：${date.name}</p>
+            <p>フォロワー数：${date.followers}</p>
+            <p>公開リポジトリ数：${date.public_repos}</p>
+            <img src=${date.avatar_url} alt ="アイコン">`)
+        .catch(error =>
+            resultArea.innerHTML =
+            `<p> ユーザーが見つかりません</p>`
+        );
+}
+
+
+
+
+
+// HTML要素への参照を取得します（入力欄、ボタン、表示領域）。
+// ボタン要素にイベントリスナーを設定し、クリックされた際に特定の関数を実行するようにします。
+// イベントリスナーで実行する関数を作成します。この関数内で以下の処理を行います。
+// 入力欄からユーザー名を取得します。
+// GitHub APIのエンドポイント（https://api.github.com/users/{ユーザー名}）を組み立てます。
+// fetch APIを使って、GitHub APIにリクエストを送信します。
+// APIからのレスポンスを処理します（JSON形式で取得）。
+// 取得したユーザー情報をHTMLの表示領域に表示します。ユーザー名、名前、フォロワー数、
+// 公開リポジトリ数、プロフィール画像
+// エラーハンドリングを行います（APIリクエストの失敗など）。
+
+
+
+
+
